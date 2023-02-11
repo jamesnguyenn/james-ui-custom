8943f8df5175017dd09f46edb1690ddf62de246e
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2023-02-11 14:31:24 +0700

    Fix Bug

--- next.config.js
+++ next.config.js
@@ -9,11 +9,13 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = {
+    nextConfig,
+};
 // const withTypeScript = require('next-tsc');
 // module.exports = withTypeScript();
 
-// // next.config.js
-// const withTM = require('next-transpile-modules')(['james-ui-custom']); // pass the modules you would like to see transpiled
+// next.config.js
+const withTM = require('next-transpile-modules')(['james-ui-custom']); // pass the modules you would like to see transpiled
 
-// module.exports = withTM({});
+module.exports = withTM({});
